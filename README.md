dae55d1be79d0c145f7e2c08ac186a395ba72623
George <georgerosephone@gmail.com>
2020-04-27 16:06:04 +0100

    V0.1
    Built a very base line application which holds the PDF Service Manuals for a couple of devices (Currently all Gen HP 250 Laptops and the Lenovo T480)
    
    This app will grow quite fast as I can update regularly with new documents, eventually the apps design overall will change in to something more fluid this was just uploaded with the baseline needs of the application met.
    
    TODO:
    - More service manuals for varying product and companies.
    
    - A better overall design and flow as you move around the application.

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # Service-Manual-Library
+
+<br />
+<p align="center">
+  <a href="https://github.com/Barophobia/repo">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">Service Manual Library</h3>
+
+  <p align="center">
+    The go to app for technical hardware service manuals (Servers,Laptops,Printers,Desktops,Tablets.)
+    <br />
+    <a href="https://github.com/Barophobia/Service-manual-library"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    ·
+    <a href="https://github.com/Barophobia/Service-manual-library/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/Barophobia/service-manual-library/issues">Request Feature</a>
+  </p>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+* [Getting Started](#getting-started)
+  * [Prerequisites](#prerequisites)
+  * [Installation](#installation)
+* [Usage](#usage)
+* [Roadmap](#roadmap)
+* [Contributing](#contributing)
+* [License](#license)
+* [Contact](#contact)
+* [Acknowledgements](#acknowledgements)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+    I built this with because I am a tech engineer who is always struggling to find the latest Service manuals for the range of products I support so I guess other people do as well, maybe. I think everyone should have the right to repair their goods and this was made supporting that.
+
+
+### Built With
+
+* Python
+* PyQT5
+* QDarkstyle
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+Currently I wouldn't recommend using this over some other resources on the web but once the application is worth while this will be updated.
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Use this however you please.
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/Barophobia/Service-manual-library/issues) for a list of proposed features (and known issues).
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Your Name - [@Barophobia2](https://twitter.com/Barophobia2) - georgerosephone@gmail.com
+
+Project Link: [https://github.com/Barophobia/Service-manual-library](https://github.com/Barophobia/service-manual-library)
+
---
+++ main.py
@@ -0,0 +1,335 @@
+import sys
+import qdarkstyle
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QPushButton, QToolTip, QMessageBox, QLabel, QMenu
+import webbrowser
+
+#HP WINDOWS
+class Hpmain(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("HP")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+        
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.label = QLabel('Different generations of a product are under the overall models',self)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.move(175,50)
+        self.label.adjustSize()
+
+        self.pushButton = QPushButton("Laptops", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>HP Laptops</h3>")
+        self.pushButton.clicked.connect(self.hplap)
+
+        self.hpmain()
+
+    def hpmain(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def back(self):
+        self.w = Window()
+        self.w.show()
+        self.hide()
+
+    def hplap(self):
+        self.w = Hplap()
+        self.w.show()
+        self.hide()
+
+class Hplap(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("HP Laptop Docs")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("250", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>HP 250 Notebook PC</h3>")
+        self.pushButton.clicked.connect(self.hp250)
+        
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.hplap()
+
+    def hplap(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def back(self):
+        self.w = Hpmain()
+        self.w.show()
+        self.hide()
+
+    def hp250(self):
+        self.w = Hp250()
+        self.w.show()
+        self.hide()
+
+
+class Hp250(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("HP 250 Docs")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+
+        self.pushButton = QPushButton("250 G1 HMM", self)
+        self.pushButton.move(375, 50)
+        self.pushButton.setToolTip("<h3>250 G1 Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.hp250g1hmm)
+
+        self.pushButton = QPushButton("250 G2 HMM", self)
+        self.pushButton.move(375, 100)
+        self.pushButton.setToolTip("<h3>250 G2 Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.hp250g2hmm)
+
+        self.pushButton = QPushButton("250 G3 HMM", self)
+        self.pushButton.move(375, 150)
+        self.pushButton.setToolTip("<h3>250 G3 Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.hp250g3hmm)
+
+        self.pushButton = QPushButton("250 G4 HMM", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>250 G4 Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.hp250g4hmm)
+
+        self.pushButton = QPushButton("250 G5 HMM", self)
+        self.pushButton.move(375, 250)
+        self.pushButton.setToolTip("<h3>250 G5 Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.hp250g5hmm)
+
+        self.pushButton = QPushButton("250 G6 HMM", self)
+        self.pushButton.move(375, 300)
+        self.pushButton.setToolTip("<h3>250 G6 Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.hp250g6hmm)
+        
+        self.pushButton = QPushButton("250 G7 HMM", self)
+        self.pushButton.move(375, 350)
+        self.pushButton.setToolTip("<h3>250 G7 Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.hp250g7hmm)
+
+
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.hp250()
+
+    def hp250g1hmm(self):
+        webbrowser.open_new(r'.\Docs\HP\Laptops\250\250g1_hmm.pdf')
+
+    def hp250g2hmm(self):
+        webbrowser.open_new(r'.\Docs\HP\Laptops\250\250g2_hmm.pdf')
+
+    def hp250g3hmm(self):
+        webbrowser.open_new(r'.\Docs\HP\Laptops\250\250g3_hmm.pdf')
+
+    def hp250g4hmm(self):
+        webbrowser.open_new(r'.\Docs\HP\Laptops\250\250g4_hmm.pdf')
+
+    def hp250g5hmm(self):
+        webbrowser.open_new(r'.\Docs\HP\Laptops\250\250g5_hmm.pdf')
+
+    def hp250g6hmm(self):
+        webbrowser.open_new(r'.\Docs\HP\Laptops\250\250g6_hmm.pdf')
+    
+    def hp250g7hmm(self):
+        webbrowser.open_new(r'.\Docs\HP\Laptops\250\250g7_hmm.pdf')
+
+    def back(self):
+        self.w = Hplap()
+        self.w.show()
+        self.hide()
+
+    def hp250(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+        self.hide()
+
+#LENOVO DESIGNS
+class Lenovomain(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Lenovo")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("Laptop", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>Lenovo Laptop Docs</h3>")
+        self.pushButton.clicked.connect(self.lenovolap)
+
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.lenovomain()
+
+    def lenovomain(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def back(self):
+        self.w = Window()
+        self.w.show()
+        self.hide()
+
+    def lenovolap(self):
+        self.w = Lenovolap()
+        self.w.show()
+        self.hide()
+
+
+class Lenovolap(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Lenovo Laptop Docs")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("T480", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>T480</h3>")
+        self.pushButton.clicked.connect(self.lenovot480)
+        
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.lenovolap()
+
+    def lenovolap(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def back(self):
+        self.w = Lenovomain()
+        self.w.show()
+        self.hide()
+
+    def lenovot480(self):
+        self.w = Lenovot480()
+        self.w.show()
+        self.hide()
+
+
+class Lenovot480(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Lenovo T480 Docs")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("Hardware Manual", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.t480hmm)
+
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.lenovot480()
+
+    def t480hmm(self):
+        webbrowser.open_new(r'.\Docs\Lenovo\Laptops\T480\t480_hmm.pdf')
+
+    def back(self):
+        self.w = Lenovolap()
+        self.w.show()
+        self.hide()
+
+    def lenovot480(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+        self.hide()
+
+#FIRST WINDOW
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.title = "Service Manual Library"
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("HP", self)
+        self.pushButton.move(205, 200)
+        self.pushButton.setToolTip("<h3>HP Docs</h3>")
+        self.pushButton.clicked.connect(self.hpmain)
+
+        self.pushButton = QPushButton("Lenovo", self)
+        self.pushButton.move(405, 200)
+        self.pushButton.setToolTip("<h3>Lenovo Docs</h3>")
+        self.pushButton.clicked.connect(self.lenovomain)
+
+        self.pushButton = QPushButton("Exit", self)
+        self.pushButton.move(300, 440)
+        self.pushButton.setToolTip("<h3>Lenovo Docs</h3>")
+        self.pushButton.clicked.connect(self.mainexit)
+
+        self.statusBar().showMessage('V0.1, Created by George Rose')
+
+        self.main_window()
+
+    def main_window(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def hpmain(self):
+        self.w = Hpmain()
+        self.w.show()
+        self.hide()
+
+    def lenovomain(self):
+        self.w = Lenovomain()
+        self.w.show()
+        self.hide()
+
+    def mainexit(self):
+        sys.exit()
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    window = Window()
+    sys.exit(app.exec())
