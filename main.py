c74d8d4c0ee6d8c2c70cdfa972bbfe1d653fa9bd
George <georgerosephone@gmail.com>
2020-05-05 12:14:54 +0100

    Update.

--- main.py
+++ main.py
@@ -95,37 +95,37 @@ class Hp250(QMainWindow):
 
 
         self.pushButton = QPushButton("250 G1 HMM", self)
-        self.pushButton.move(375, 50)
+        self.pushButton.move(300, 50)
         self.pushButton.setToolTip("<h3>250 G1 Hardware Maintenance Manual</h3>")
         self.pushButton.clicked.connect(self.hp250g1hmm)
 
         self.pushButton = QPushButton("250 G2 HMM", self)
-        self.pushButton.move(375, 100)
+        self.pushButton.move(300, 100)
         self.pushButton.setToolTip("<h3>250 G2 Hardware Maintenance Manual</h3>")
         self.pushButton.clicked.connect(self.hp250g2hmm)
 
         self.pushButton = QPushButton("250 G3 HMM", self)
-        self.pushButton.move(375, 150)
+        self.pushButton.move(300, 150)
         self.pushButton.setToolTip("<h3>250 G3 Hardware Maintenance Manual</h3>")
         self.pushButton.clicked.connect(self.hp250g3hmm)
 
         self.pushButton = QPushButton("250 G4 HMM", self)
-        self.pushButton.move(375, 200)
+        self.pushButton.move(300, 200)
         self.pushButton.setToolTip("<h3>250 G4 Hardware Maintenance Manual</h3>")
         self.pushButton.clicked.connect(self.hp250g4hmm)
 
         self.pushButton = QPushButton("250 G5 HMM", self)
-        self.pushButton.move(375, 250)
+        self.pushButton.move(300, 250)
         self.pushButton.setToolTip("<h3>250 G5 Hardware Maintenance Manual</h3>")
         self.pushButton.clicked.connect(self.hp250g5hmm)
 
         self.pushButton = QPushButton("250 G6 HMM", self)
-        self.pushButton.move(375, 300)
+        self.pushButton.move(300, 300)
         self.pushButton.setToolTip("<h3>250 G6 Hardware Maintenance Manual</h3>")
         self.pushButton.clicked.connect(self.hp250g6hmm)
         
         self.pushButton = QPushButton("250 G7 HMM", self)
-        self.pushButton.move(375, 350)
+        self.pushButton.move(300, 350)
         self.pushButton.setToolTip("<h3>250 G7 Hardware Maintenance Manual</h3>")
         self.pushButton.clicked.connect(self.hp250g7hmm)
 
@@ -215,9 +215,24 @@ class Lenovolap(QMainWindow):
         self.left = 100
         self.width = 680
         self.height = 500
+        
+        self.pushButton = QPushButton("T450", self)
+        self.pushButton.move(150, 100)
+        self.pushButton.setToolTip("<h3>T450</h3>")
+        self.pushButton.clicked.connect(self.lenovot450)
+
+        self.pushButton = QPushButton("T460", self)
+        self.pushButton.move(250, 100)
+        self.pushButton.setToolTip("<h3>T460</h3>")
+        self.pushButton.clicked.connect(self.lenovot460)
+
+        self.pushButton = QPushButton("T470", self)
+        self.pushButton.move(350, 100)
+        self.pushButton.setToolTip("<h3>T470</h3>")
+        self.pushButton.clicked.connect(self.lenovot470)
 
         self.pushButton = QPushButton("T480", self)
-        self.pushButton.move(375, 200)
+        self.pushButton.move(450, 100)
         self.pushButton.setToolTip("<h3>T480</h3>")
         self.pushButton.clicked.connect(self.lenovot480)
         
@@ -242,6 +257,126 @@ class Lenovolap(QMainWindow):
         self.w.show()
         self.hide()
 
+    def lenovot470(self):
+        self.w = Lenovot470()
+        self.w.show()
+        self.hide()
+
+    def lenovot460(self):
+        self.w = Lenovot460()
+        self.w.show()
+        self.hide()
+
+    def lenovot450(self):
+        self.w = Lenovot450()
+        self.w.show()
+        self.hide()
+
+class Lenovot450(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Lenovo T450 Docs")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("Hardware Manual", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.t450hmm)
+
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.lenovot450()
+
+    def t450hmm(self):
+        webbrowser.open_new(r'.\Docs\Lenovo\Laptops\T450\t450_hmm.pdf')
+
+    def back(self):
+        self.w = Lenovolap()
+        self.w.show()
+        self.hide()
+
+    def lenovot450(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+        self.hide()
+
+
+class Lenovot460(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Lenovo T460 Docs")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("Hardware Manual", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.t460hmm)
+
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.lenovot460()
+
+    def t460hmm(self):
+        webbrowser.open_new(r'.\Docs\Lenovo\Laptops\T460\t460_hmm.pdf')
+
+    def back(self):
+        self.w = Lenovolap()
+        self.w.show()
+        self.hide()
+
+    def lenovot460(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+        self.hide()
+
+class Lenovot470(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Lenovo T470 Docs")
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.pushButton = QPushButton("Hardware Manual", self)
+        self.pushButton.move(375, 200)
+        self.pushButton.setToolTip("<h3>Hardware Maintenance Manual</h3>")
+        self.pushButton.clicked.connect(self.t470hmm)
+
+        self.pushButton = QPushButton("Back", self)
+        self.pushButton.move(300, 450)
+        self.pushButton.setToolTip("<h3>Go back a page</h3>")
+        self.pushButton.clicked.connect(self.back)
+
+        self.lenovot470()
+
+    def t470hmm(self):
+        webbrowser.open_new(r'.\Docs\Lenovo\Laptops\T470\t470_hmm.pdf')
+
+    def back(self):
+        self.w = Lenovolap()
+        self.w.show()
+        self.hide()
+
+    def lenovot470(self):
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+        self.hide()
 
 class Lenovot480(QMainWindow):
     def __init__(self):
